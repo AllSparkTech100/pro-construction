a549341fbd56d40b0af9b9b61c5af3f3b43bb69b
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2023-04-09 12:24:02 +0100

    Updates

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function showDivs(n) {
     let x = document.getElementsByClassName('slides');
     if (n > x.length) { slideIndex = 1 };
     if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length;) {
+    for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
 
