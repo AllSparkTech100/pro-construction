142971309352ca541b820c51735a5b38e6a5e811
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2023-04-09 12:21:28 +0100

    Updates on site

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 let logo = document.querySelector('.wrapper-inner');
 let menu = document.querySelector(".menu");
 
-logo.addEventListener("click", function (){
+logo.addEventListener("click", function () {
     menu.classList.toggle('showmenu');
 });
+
+
+
+//slider
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName('slides');
+    if (n > x.length) { slideIndex = 1 };
+    if (n < 1) { slideIndex = x.length };
+    for (i = 0; i < x.length;) {
+        x[i].style.display = "none";
+    }
+
+    x[slideIndex - 1].style.display = "block";
+}
