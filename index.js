6acc2f9059b2c046fdd62aff37678e12ec67e441
AllSparkTech100 <105206727+AllSparkTech100@users.noreply.github.com>
2023-04-22 16:28:44 +0100

    Additional Updates

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// let logo = document.querySelector('.wrapper-inner');
-// let menu = document.querySelector(".menu");
-
-// logo.addEventListener("click", function () {
-//     menu.classList.toggle('showmenu');
-// });
-
-
-
 //slider
 let slideIndex = 1;
 showDivs(slideIndex);
